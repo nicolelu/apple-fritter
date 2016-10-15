471879240e5b0a91524674d1957a4144d7d377f7
Nicole Lu <nicolelu@mit.edu>
2016-10-15 16:27:33 -0400

    Changed the app.listen port to use the environmental port

--- app.js
+++ app.js
@@ -93,6 +93,7 @@ app.post("/freets", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port 3000");
 });
+

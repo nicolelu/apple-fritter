c951a504f05af9083a5564e74f0d397b1ae73983
Nicole Lu <nicolelu@mit.edu>
2016-10-17 22:32:12 -0400

    removed unnecessary files

--- public/index.js
+++
@@ -1,9 +0,0 @@
-$(document).on("click", "#postfreet", function() {
-  // On create button click, POST to /links to make shortening and then redirect to URL.
-  $.post("/makefreet", {
-    "username": $("#username").val(),
-    "freet": $("#freet").val(),
-  }, function(data) {
-    alert("Posted freet");
-  });
-});
